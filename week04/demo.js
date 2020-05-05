7e6aab2e24f0a83a040c212dbda4fd5aa6b344ed
liuhuijuan_home <1140409061@qq.com>
2020-05-05 18:53:56 +0800

    week04补充

--- week04/demo.js
+++ week04/demo.js
@@ -24,3 +24,23 @@ setTimeout(function(){
 console.log("4");
 console.log("5");
 afoo()
+
+
+new Promise(res=>res()).then(()=>setTimeout(()=>console.log(1),10000),console.log(0));
+console.log(2);
+
+async function async1() {
+    console.log('async1 start');
+    await async2();
+    console.log('async1 end');
+}
+async function async2() {
+    console.log('async2');
+}
+async1();
+new Promise(function (resolve) {
+    console.log('promise1');
+    resolve();
+}).then(function () {
+    console.log('promise2');
+});
